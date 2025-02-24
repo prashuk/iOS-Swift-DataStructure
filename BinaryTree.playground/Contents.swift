ac5a2962b5a47eb08c2cd9c324e2bb76e1022c8a
Prashuk <prashuk.ajmera@gmail.com>
2025-02-23 20:46:26 -0800

    level order updated

--- BinaryTree.playground/Contents.swift
+++ BinaryTree.playground/Contents.swift
@@ -35,58 +35,90 @@ class BinaryTree {
         print(node.val, terminator: " ")
     }
     
-    func levelOrder(_ node: Node?) { // BFS
-        guard let node else { return }
-        var queue: [Node] = [node]
+    func levelOrder(_ node: Node?) -> [[Int]] { // BFS
+        guard let node else { return [] }
         
-        while !queue.isEmpty {
-            let newNode = queue.removeFirst()
-            print(newNode.val, terminator: " ")
-            
-            if let node = newNode.left { queue.append(node) }
-            if let node = newNode.right { queue.append(node) }
-        }
-    }
-    
-    func zigZagOrder(_ node: Node?) {
-        guard let node else { return }
         var queue: [Node] = [node]
-        var leftToRigh = false
+        var order: [[Int]] = []
         
         while !queue.isEmpty {
             var levelValues = [Int]()
+            let temp = queue
+            queue.removeAll()
             
-            for i in 0..<queue.count {
-                let newNode = queue.removeFirst()
+            for node in temp {
+                levelValues.append(node.val)
                 
-                if leftToRigh {
-                    levelValues.append(newNode.val)
+                if let left = node.left { queue.append(left) }
+                if let right = node.right { queue.append(right) }
+            }
+            
+            order.append(levelValues)
+        }
+        
+        return order
+    }
+    
+    func zigZagOrder(_ node: Node?) -> [[Int]] {
+        guard let node else { return [] }
+        
+        var queue: [Node] = [node]
+        var order: [[Int]] = []
+        var leftToRight = false
+        
+        while !queue.isEmpty {
+            var levelValues = [Int]()
+            let temp = queue
+            queue.removeAll()
+            
+            for node in temp {
+                if leftToRight {
+                    levelValues.append(node.val)
                 } else {
-                    levelValues.insert(newNode.val, at: 0)
+                    levelValues.insert(node.val, at: 0)
                 }
                 
-                if let node = newNode.left { queue.append(node) }
-                if let node = newNode.right { queue.append(node) }
+                if let left = node.left { queue.append(left) }
+                if let right = node.right { queue.append(right) }
             }
-            print(levelValues.map { String($0) }.joined(separator: " "), terminator: " ")
-            leftToRigh.toggle()
+            
+            order.append(levelValues)
+            
+            leftToRight.toggle()
         }
+        
+        return order
     }
 }
 
 let tree = BinaryTree()
 tree.root = Node(1)
+
 tree.root?.left = Node(2)
 tree.root?.right = Node(3)
+
 tree.root?.left?.left = Node(4)
 tree.root?.left?.right = Node(5)
+tree.root?.right?.left = Node(6)
+tree.root?.right?.right = Node(7)
+
+tree.root?.left?.left?.left = Node(8)
+tree.root?.left?.left?.right = Node(9)
+tree.root?.left?.right?.left = Node(10)
+tree.root?.left?.right?.right = Node(11)
+tree.root?.right?.left?.left = Node(12)
+tree.root?.right?.left?.right = Node(13)
+tree.root?.right?.right?.left = Node(14)
+tree.root?.right?.right?.right = Node(15)
 
 tree.inOrder(tree.root)
 print()
+
 tree.preOrder(tree.root)
 print()
+
 tree.postOrder(tree.root)
 print()
-tree.levelOrder(tree.root)
-print()
-tree.zigZagOrder(tree.root)
+
+print(tree.levelOrder(tree.root))
+print(tree.zigZagOrder(tree.root))
